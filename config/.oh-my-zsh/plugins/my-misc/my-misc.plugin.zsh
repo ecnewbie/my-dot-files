a10623c479cfada55c7aefcbf5716eddb0ae6043
ecnewbie <ecnewbie@gmail.com>
2015-10-16 18:02:02 +0800

    rearrange.

---
+++ config/.oh-my-zsh/plugins/my-misc/my-misc.plugin.zsh
@@ -0,0 +1,34 @@
+alias ftmp='find . -name "*[~#]"'
+alias ftmpr='find . -name "*[~#]" | xargs rm'
+# find content in all files, ignore dot files.
+alias ffct='find . -type f | sed  -e "/\/\./ d" | xargs grep -n'
+
+openf () {
+    if [ "$1" = "" ] ;then
+	xdg-open `(find . | percol)`
+    else
+	xdg-open $1
+    fi
+}
+
+search () {
+    w3m "http://en.cppreference.com/mwiki/index.php?title=Special%3ASearch&search=$1"
+}
+
+# find content in files which match pattern, ignore dot files.
+ffctp () {
+    find . -type f -name "$1" | sed  -e "/\/\./ d" | xargs grep -n "$2"
+}
+
+# filt file and show tail 20 lines.
+filtfv () {
+    sed -n "/$1/"p $2 | tail -n 20
+}
+
+filtfva () {
+    sed -n "/$1/"p $2 | less
+}
+
+filtfa () {
+    sed -n "/$1/"p $2
+}
